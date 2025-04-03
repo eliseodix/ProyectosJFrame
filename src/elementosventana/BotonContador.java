4bb117336c50798f4145efdc9115160266a08c95
unknown <eliseogracias776@gmail.com>
2025-04-03 17:14:21 -0500

    Reutilizacion

---
+++ src/elementosventana/BotonContador.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elementosventana;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author usuario
+ */
+public class BotonContador extends JButton{
+    int pulsaciones;
+    
+    public BotonContador(){
+        pulsaciones=0;
+    }
+    
+    
+    public void setpulsaciones(int p){
+        pulsaciones =p;
+    }
+    public int getpulsaciones(){
+        return pulsaciones;
+    }
+    public void incrementa(){
+        pulsaciones++;
+    }
+    public void decrementa(){
+        pulsaciones--;
+    }
+    public void reiniciar(){
+        pulsaciones=0;
+    }
+    public void aumenta(int c){
+        pulsaciones = pulsaciones+c;
+    }
+    public void disminuye(int c){
+        pulsaciones = pulsaciones-c;
+    }
+    public void incrementaC(){
+        pulsaciones+=2;
+    }
+}
