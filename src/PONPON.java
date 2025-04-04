73e1c347ea4768524220b89e24e7a87b22aab2b6
unknown <eliseogracias776@gmail.com>
2025-04-04 11:38:52 -0500

    VectoresVeventos

---
+++ src/PONPON.java
@@ -0,0 +1,60 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PONPON {
+    
+    
+    public static int  login(String susario, String contra){
+        int P=0;
+        if (susario.equals("dd")&& contra.equals("ee")){
+            P=1;
+        }else{
+            P=0;
+        }
+       
+        
+        
+       return P;
+    }
+    public static double mater(double A, double B){
+        double operacion=0;
+       double ados=0;
+       double bdos=0;
+       
+       Math.pow(bdos, 2);
+        Math.pow(ados, 2);
+        A=Math.pow(A, 3);
+        B=Math.pow( B, 3);
+        operacion=A+3*ados*B+3*A*bdos+B;
+      
+         return operacion;
+        
+        
+        
+        
+        
+                   }
+         
+         
+      
+        
+       
+        
+        
+        
+      
+        
+    
+    
+            
+    
+}
